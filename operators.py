bf3282e961a76024130016170170956405061863
lavishj2025-ctrl <lavishj2025@gmail.com>
2026-09-02 08:55:07 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,26 @@
+#arithmetic operators 
+a=5
+b=2
+sum=a+b
+print(sum)
+
+#relational operators
+a=50
+b=20
+print(a==b) #false
+print(a!=b) #true
+print(a>=b) #true
+print(a>b)  #true
+print(a<=b) #false
+print(a<b)  #false
+
+#assignment operator
+num=10
+num=num+10  #num+=10
+print(num)
+
+#logical operators
+a=50
+b=30
+print(not False)
+print(not(a>b))
